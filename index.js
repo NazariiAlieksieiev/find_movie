e83c083d8611333d612294eacca2126f2635f431
Nazarii AlieKsieiev <nazarmetall@gmail.com>
2023-07-23 16:26:04 +0300

    Try to deploy project

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
 const express = require('express');
 const cors = require('cors');
-const rateLimit = require('express-rate-limit')
 require('dotenv').config()
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+//Enable cors
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true 
+}));
+
 //Routes
 app.use('/find_movie', require('./routes/index'))
 
 // Set static folder
 app.use(express.static('public'));
-
-
-//Enable cors
-app.use(cors({
-  origin: 'http://localhost:5000/find_movie',
-  credentials: true 
-}));
 
 app.listen(PORT, () => console.log(`Server runnig on port ${PORT}`))
